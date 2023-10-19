7037c47dc559fcc5d5858ed1c9c37a6acf3538d3
donl0 <donl_117n@mail.ru>
2023-10-19 19:24:59 +0300

    Layout adaptation

--- app/src/main/java/com/example/mvp_example/code/asset_table/model/AllAssetsTableStubModel.kt
+++ app/src/main/java/com/example/mvp_example/code/asset_table/model/AllAssetsTableStubModel.kt
@@ -6,12 +6,12 @@ import com.example.mvp_example.code.asset_table.model.objects.AssetTable
 class AllAssetsTableStubModel: IAssetTableModel {
     override fun takeData(): ArrayList<AssetTable> {
         val items: ArrayList<AssetTable> = arrayListOf(
-            AssetTable(R.drawable.anet, "Arista\nNetwork Inc.", "ANET", 0f, 18836.79f, 12145.60f, 14.43f),
+            AssetTable(R.drawable.anet, "Arista\nNetwork Inc.", "ANET", 0f, 18836.79f, 1215.60f, 14.43f),
             AssetTable(R.drawable.mnst, "Monster\nBeverage\nCorporation", "MNST", 0f, 4856.85f, 207.41f, 14.43f),
-            AssetTable(R.drawable.lvs, "Las Vegas\nSands Corp.", "LVS", 116f, 4516.87f, 33657.51f, 7.34f),
-            AssetTable(R.drawable.hsy, "The Hershey\nCompany", "HSY", 26f, 18717.38f, -22188.98f, -4.80f),
-            AssetTable(R.drawable.btc, "Bitcoin", "BTC", 0.0352f, 2764120.04f, 36778.85f, 111.89f),
-            AssetTable(R.drawable.etherium, "Etherium", "ETH", 0.4386f, 2768123.78f, 120521.81f, 205.15f),
+            AssetTable(R.drawable.lvs, "Las Vegas\nSands Corp.", "LVS", 116f, 4516.87f, 3367.51f, 7.34f),
+            AssetTable(R.drawable.hsy, "The Hershey\nCompany", "HSY", 26f, 18717.38f, -2218.98f, -4.80f),
+            AssetTable(R.drawable.btc, "Bitcoin", "BTC", 0.0352f, 27410.04f, 36778.85f, 111.89f),
+            AssetTable(R.drawable.etherium, "Etherium", "ETH", 0.4386f, 27681.78f, 12051.81f, 205.15f),
             AssetTable(R.drawable.matic, "Polygon", "MATIC", 0.062f, 49.58f, -0.01f, -47.67f),
             AssetTable(R.drawable.sol, "Solana", "SOL", 10f, 2360.54f, 7967.88f, 50.95f),
             AssetTable(R.drawable.usdt, "Tether", "USDT", 132.016f, 97.14f, 8676.22f, 41.02f),
--- app/src/main/java/com/example/mvp_example/code/asset_table/model/CryptoTableModel.kt
+++ app/src/main/java/com/example/mvp_example/code/asset_table/model/CryptoTableModel.kt
@@ -6,8 +6,8 @@ import com.example.mvp_example.code.asset_table.model.objects.AssetTable
 class CryptoTableModel: IAssetTableModel {
     override fun takeData(): ArrayList<AssetTable> {
         val items: ArrayList<AssetTable> = arrayListOf(
-            AssetTable(R.drawable.btc, "Bitcoin", "BTC", 0.0352f, 2764120.04f, 36778.85f, 111.89f),
-            AssetTable(R.drawable.etherium, "Etherium", "ETH", 0.4386f, 2768123.78f, 120521.81f, 205.15f),
+            AssetTable(R.drawable.btc, "Bitcoin", "BTC", 0.0352f, 2764.04f, 3678.85f, 111.89f),
+            AssetTable(R.drawable.etherium, "Etherium", "ETH", 0.4386f, 27683.78f, 1201.81f, 205.15f),
             AssetTable(R.drawable.matic, "Polygon", "MATIC", 0.062f, 49.58f, -0.01f, -47.67f),
             AssetTable(R.drawable.sol, "Solana", "SOL", 10f, 2360.54f, 7967.88f, 50.95f),
             AssetTable(R.drawable.usdt, "Tether", "USDT", 132.016f, 97.14f, 8676.22f, 41.02f),
