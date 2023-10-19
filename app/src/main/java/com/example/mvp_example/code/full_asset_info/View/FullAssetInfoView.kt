d2b6db7104b29700c7063fe8ee5f59f766741aae
donl0 <donl_117n@mail.ru>
2023-10-19 20:21:23 +0300

    Add folders for full asset info

---
+++ app/src/main/java/com/example/mvp_example/code/full_asset_info/View/FullAssetInfoView.kt
@@ -0,0 +1,12 @@
+package com.example.mvp_example.code.full_asset_info.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvp_example.R
+
+class FullAssetInfoView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_full_asset_info)
+    }
+}
